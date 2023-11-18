e07e8d7195d4846083f70df7358974d2b24dbf62
Ruan Silva <ruanluiz.as@gmail.com>
2023-11-17 22:54:01 -0300

    criacao do builder de response e implementacao da logica do storageprocess

--- internal/v1/gainprojection/builder.go
+++ internal/v1/gainprojection/builder.go
@@ -1,22 +1,125 @@
 package gainprojection
 
+import "time"
+
+type GainProjectionBuilder struct {
+	id          string
+	createdAt   time.Time
+	payIn       time.Time
+	description string
+	value       float64
+	isPassive   bool
+	isDone      bool
+	userId      string
+	category    GainCategory
+}
+
 func NewGainProjectionBuilder() *GainProjectionBuilder {
 	return &GainProjectionBuilder{}
 }
-
-// TODO: Terminar o builder. Inserir os outros campos do GainProjection
-type GainProjectionBuilder struct {
-	id string
-}
-
 func (builder *GainProjectionBuilder) AddId(id string) *GainProjectionBuilder {
 	builder.id = id
 	return builder
 }
-
+func (builder *GainProjectionBuilder) AddCreatedAt(createdAt time.Time) *GainProjectionBuilder {
+	builder.createdAt = createdAt
+	return builder
+}
+func (builder *GainProjectionBuilder) AddPayIn(payIn time.Time) *GainProjectionBuilder {
+	builder.payIn = payIn
+	return builder
+}
+func (builder *GainProjectionBuilder) AddDescription(description string) *GainProjectionBuilder {
+	builder.description = description
+	return builder
+}
+func (builder *GainProjectionBuilder) AddValue(value float64) *GainProjectionBuilder {
+	builder.value = value
+	return builder
+}
+func (builder *GainProjectionBuilder) AddIsPassive(isPassive bool) *GainProjectionBuilder {
+	builder.isPassive = isPassive
+	return builder
+}
+func (builder *GainProjectionBuilder) AddIsDone(isDone bool) *GainProjectionBuilder {
+	builder.isDone = isDone
+	return builder
+}
+func (builder *GainProjectionBuilder) AddUserId(userId string) *GainProjectionBuilder {
+	builder.userId = userId
+	return builder
+}
+func (builder *GainProjectionBuilder) AddCategory(category GainCategory) *GainProjectionBuilder {
+	builder.category = category
+	return builder
+}
 func (builder *GainProjectionBuilder) Build() *GainProjection {
 	gainProjection := GainProjection{}
+
 	gainProjection.Id = builder.id
+	gainProjection.CreatedAt = builder.createdAt
+	gainProjection.PayIn = builder.payIn
+	gainProjection.Description = builder.description
+	gainProjection.Value = builder.value
+	gainProjection.IsPassive = builder.isPassive
+	gainProjection.IsDone = builder.isDone
+	gainProjection.UserId = builder.userId
+	gainProjection.Category = builder.category
 
 	return &gainProjection
+}
+
+type GainProjectionResponseBuilder struct {
+	id          string
+	payIn       time.Time
+	description string
+	value       float64
+	isPassive   bool
+	recurrence  uint
+	category    CategoryResponse
+}
+
+func NewGainProjectionResponseBuilder() *GainProjectionResponseBuilder {
+	return &GainProjectionResponseBuilder{}
+}
+func (builder *GainProjectionResponseBuilder) AddId(id string) *GainProjectionResponseBuilder {
+	builder.id = id
+	return builder
+}
+func (builder *GainProjectionResponseBuilder) AddPayIn(payIn time.Time) *GainProjectionResponseBuilder {
+	builder.payIn = payIn
+	return builder
+}
+func (builder *GainProjectionResponseBuilder) AddDescription(description string) *GainProjectionResponseBuilder {
+	builder.description = description
+	return builder
+}
+func (builder *GainProjectionResponseBuilder) AddValue(value float64) *GainProjectionResponseBuilder {
+	builder.value = value
+	return builder
+}
+func (builder *GainProjectionResponseBuilder) AddIsPassive(isPassive bool) *GainProjectionResponseBuilder {
+	builder.isPassive = isPassive
+	return builder
+}
+func (builder *GainProjectionResponseBuilder) AddRecurrence(recurrence uint) *GainProjectionResponseBuilder {
+	builder.recurrence = recurrence
+	return builder
+}
+func (builder *GainProjectionResponseBuilder) AddCategory(category CategoryResponse) *GainProjectionResponseBuilder {
+	builder.category = category
+	return builder
+}
+func (builder *GainProjectionResponseBuilder) Build() *GainProjectionResponse {
+	gainProjectionResponse := GainProjectionResponse{}
+
+	gainProjectionResponse.Id = builder.id
+	gainProjectionResponse.Description = builder.description
+	gainProjectionResponse.Value = builder.value
+	gainProjectionResponse.PayIn = builder.payIn
+	gainProjectionResponse.IsPassive = builder.isPassive
+	gainProjectionResponse.Recurrence = builder.recurrence
+	gainProjectionResponse.Category = builder.category
+
+	return &gainProjectionResponse
 }
--- internal/v1/gainprojection/storageprocess.go
+++ internal/v1/gainprojection/storageprocess.go
@@ -1,6 +1,10 @@
 package gainprojection
 
-import uuid "github.com/satori/go.uuid"
+import (
+	"time"
+
+	uuid "github.com/satori/go.uuid"
+)
 
 type StorageProcess interface {
 	Create(request CreateRequest) (*GainProjectionResponse, error)
@@ -16,5 +20,58 @@ func NewStorageProcess(repository Repository) StorageProcess {
 }
 
 func (sp *storageProcess) Create(request CreateRequest) (*GainProjectionResponse, error) {
+	createdAt := time.Now()
+	gainProjection := NewGainProjectionBuilder().
+		AddId(sp.generateUUID().String()).
+		AddCreatedAt(createdAt).
+		AddPayIn(request.PayIn).
+		AddIsPassive(request.IsPassive).
+		AddIsDone(false).
+		AddCategory(GainCategory{Id: request.CategoryId}).
+		AddDescription(request.Description).
+		AddValue(request.Value).
+		AddUserId("User1").
+		Build()
 
+	gainProjectionSaved, err := sp.repository.Save(*gainProjection)
+	if err != nil {
+		return nil, err
+	}
+
+	if request.Recurrence > 1 {
+		for i := 1; i < int(request.Recurrence+1); i++ {
+			gainProjection := NewGainProjectionBuilder().
+				AddId(sp.generateUUID().String()).
+				AddCreatedAt(createdAt).
+				AddPayIn(request.PayIn.AddDate(0, i, 0)).
+				AddIsPassive(request.IsPassive).
+				AddIsDone(false).
+				AddCategory(GainCategory{Id: request.CategoryId}).
+				AddDescription(request.Description).
+				AddValue(request.Value).
+				AddUserId("User1").
+				Build()
+
+			_, err = sp.repository.Save(*gainProjection)
+			if err != nil {
+				return nil, err
+			}
+		}
+	} else {
+		request.Recurrence = 1
+	}
+	gainProjectionSaved, err = sp.repository.GetById(gainProjectionSaved.Id)
+	if err != nil {
+		return nil, err
+	}
+
+	return NewGainProjectionResponseBuilder().
+		AddId(gainProjection.Id).
+		AddPayIn(gainProjection.PayIn).
+		AddDescription(gainProjection.Description).
+		AddValue(gainProjection.Value).
+		AddIsPassive(gainProjection.IsPassive).
+		AddCategory(CategoryResponse{Id: gainProjection.Category.Id, Category: gainProjection.Category.Category}).
+		AddRecurrence(request.Recurrence).
+		Build(), nil
 }
